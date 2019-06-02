2d48c813ab212483a4dd84740326305f11c9777c
Curtis Dunne <curtdunne@gmail.com>
2019-06-02 16:26:44 -0600

    Dev tool initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in neutrino_software_company_view_tool.gemspec
+gemspec
---
+++ lib/neutrino_software_company_view_tool.rb
@@ -0,0 +1,7 @@
+require "neutrino_software_company_view_tool/version"
+require "neutrino_software_company_view_tool/renderer"
+
+module NeutrinoSoftwareCompanyViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
